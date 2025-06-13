7f03ce6a58e8c28bbfb5045acccd4ce58f516a5a
TonyIvanoff <antonas.i@gmail.com>
2025-06-13 21:51:39 +0300

    created responsive Left Menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -5,9 +9,17 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/* LEFT   */}
-    <div className="w-[16%] md:w-[8%] lg:w-[16%] xl:w-[16%] bg-red-100">LEFT</div>
+    <div className="w-[16%] md:w-[8%] lg:w-[16%] xl:w-[16%] p-4">
+      <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+      <Image src="/logo.png" alt="logo" width={32} height={32}/>
+      <span className="hidden lg:block">School</span>
+      </Link>
+      <Menu/>
+    </div>
+    
+    
      {/* RIGHT   */}
-    <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[84%] bg-blue-100">RIGHT</div>
+    <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[84%] bg-[#F7F8FA] overflow-scroll">RIGHT</div>
     
     </div>;
 }
