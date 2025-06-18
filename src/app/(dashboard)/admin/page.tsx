3117e1970efc1fe0b976dbf5d4bc5c6d96906539
TonyIvanoff <antonas.i@gmail.com>
2025-06-18 09:46:43 +0300

    divided admin page into 3 parts. added usercards component for admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,18 @@
 const AdminPage = () => {
     return (
-        <div className="">AdminPage</div>
+        <div className="p-4 flex gap-4 flex-col md:flex-row">AdminPage
+        {/* LEFT */}
+        <div className="w-full lg:w-2/3">l</div>
+        {/* USER CARDS */}
+        <div className="">
+            
+        </div>
+        {/* RIGHT */}
+        <div className="w-full lg:w-1/3">r</div>
+        
+        </div>
+                
+
     )
 }
 
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,8 @@
+const UserCard = ({type}:{type:string}) => {
+    return (
+        <div className="">UserCard</div>
+    )
+
+}
+
+export default UserCard
