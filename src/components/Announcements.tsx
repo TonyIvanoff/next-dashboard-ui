63b797f447367cb4ee1035dbe87af858e5699f81
TonyIvanoff <antonas.i@gmail.com>
2025-06-19 15:56:02 +0300

    Created Announcements and completed Admin page

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,8 +1,38 @@
 
 const Announcements = () => {
     return (
-        <div className="">Announcements</div>
-    )
-}
+            <div className="bg-white p-4 rounded-md">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-xl font-semibold">Announcements</h1>
+                    <span className="text-xs text-gray-400">View All</span>
+                </div>
+                <div className="flex flex-col gap-4 mt-4">
+                    <div className="bg-lamaSky rounded-md p-4">
+                        <div className="flex items-center justify-between">
+                            <h2 className="font-medium">Random name</h2>
+                            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-06-19</span>
+                        </div>
+                        <p className="text-sm text-gray-400 mt-1">This is a copy of the announcement hardcoded directly</p>
+                    </div>
+                    <div className="bg-lamaSky rounded-md p-4">
+                        <div className="flex items-center justify-between">
+                            <h2 className="font-medium">Random name</h2>
+                            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-06-19</span>
+                        </div>
+                        <p className="text-sm text-gray-400 mt-1">This is a copy of the announcement hardcoded directly</p>
+                    </div>
+                    <div className="bg-lamaSky rounded-md p-4">
+                        <div className="flex items-center justify-between">
+                            <h2 className="font-medium">Random name</h2>
+                            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-06-19</span>
+                        </div>
+                        <p className="text-sm text-gray-400 mt-1">This is a copy of the announcement hardcoded directly</p>
+                    </div>
+                    
+                </div>
+            </div>
+    );
+    
+};
 
 export default Announcements
